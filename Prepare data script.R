ee517f192228b2257e8f680dd9792f602c75439f
Jon Paige <jnpaige@asu.edu>
2024-01-10 16:48:00 -0600

    Shifting to a spline model
    Also removed the code chunks that call in data from the github repo, as it likely just complicates things a bit more.

--- Prepare data script.R
+++ Prepare data script.R
@@ -1,30 +1,20 @@
-library(RCurl)
-library(here)
-library(ggplot2)
+setwd(here::here())# Load and prepare the data
+data <- read.csv("Paige and Perreault Dataset.csv", header = TRUE)
 
-#load in the appropriate version of the procedural unit dataset.
-#Generated using procedural unit codebook version 1.0
+data <- data[order(-data$Mean_Age), ]  # Sort the data by mean age. This step is necessary to generate the cumulative maxima
+d <- data.frame(
+  original_age = data$Mean_Age,
+  original_early = data$Early_Age,
+  original_late = data$Late_Age,
+  mean_age = data$Mean_Age,
+  early_age = data$Early_Age,
+  late_age = data$Late_Age,
+  PU = data$Total.PU,
+  site = data$SiteID,
+)
 
-download.file("https://raw.githubusercontent.com/jnpaige/Procedural.unit.dataset/main/Dataset/procedural%20units%20obfus.csv",
-              destfile = "procedural_units_v_1.0.csv", method = "curl")
+# Scale age measurements
+d$mean_age <- scale((d$mean_age))
+d$early_age <- scale((d$early_age))
+d$late_age <- scale((d$late_age))
 
-##
-##Clean data/select only Single chains
-##
-
-setwd(here::here())
-data<-read.csv("procedural_units_v_1.0.csv")
-data<-subset(data, Single.Chain=="Yes") # Remove rows describing PU present across all chains
-data<-subset(data, Arch==1)       #get only archaeological cases
-data<-data[,grep("Source|Sitename|KA.young|KA.old|Lat|Long|Species.attribution|Descr.|PU",names(data))]
-data$PU<-rowSums(data[,grep("PU",names(data))]) #assign a total number of procedural units to each row
-data[, grep("PU", names(data))]<-lapply(data[, grep("PU", names(data))], as.numeric)
-data <- subset(data, data$KA.young > 1) # Eliminate data points younger than 1000 years BP
-names(data)[2:4]<-c("site","Early.age","Late.age")
-
-#scale and log transform age to help model behave well.
-data$age <- scale(log10((data$Early.age+data$Late.age)/2)) # Midpoint between upper and lower bound
-data$original_age<-(data$Early.age+data$Late.age)/2
-write.csv(data, file="Paige and Perreault Dataset.csv")
-d <- read.csv("Paige and Perreault Dataset.csv", header = TRUE)
-data<-NULL
